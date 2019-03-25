074485d8939a2bebbd5a2e839609c6bd725c1be9
MariaMsu <i.knaz@yandex.ru>
2019-03-25 11:29:38 +0300

    chnge files entrails

--- HA_1/check-features.py
+++ HA_1/check-features.py
@@ -1,208 +1,130 @@
 # coding: utf-8
-import random
+#
+#
+# Test script to check quality of features extraction
+# Directory structure:
+# ./data/ <- folder with urls data files
+# ./check/ <- folder with files included correctly extracted features
+# This script will scan ./data/ folder and fetch urls from files
+# each site was placed to pair of files general and examined.
+# Each pair of files for site will be passed as parameter of 
+# the extract_features module. 
+###########
+# Rules:
+# 1. Execution of single feature extraction from coupe of site files limits by 1 sec 
+# 1. Features diff no more then: 0.05
+# 1, Features diff by count: no more then 0.1
+#######
+#
+
+import sys
+import os
 import re
-import numpy as np
-from urlparse import urlparse, parse_qs, unquote
-
-LINK_QUANTITY = 1000
-ALPHA = 0.1
+import time
+import extract_features
 
 
-# INPUT_FILE_1 - Q_Link
-def extract_features(INPUT_FILE_1, INPUT_FILE_2, OUTPUT_FILE):
-    # 1 len :
-    segments = {}
+def extract_names(f):
+    m = re.search(r'urls\.(\w+)\.(\w+)', f)
+    if m is not None:
+        return m.group(1), m.group(2), f
 
-    # 2 mane_list :
-    param_name = {}
 
-    # 3 "param=value" :
-    param = {}
+def read_feas(file_name):
+    result = {}
+    with open(file_name) as f:
+        
+        for line in f:
+            line = line.strip()
+            p = line.split('\t')
+            if len(p) > 1:
+                result[p[0]] = p[1]
+    return result            
 
-    # 4a (index, name) :
-    segment_name = {}
 
-    # 4b index : 1
-    segment_0_9 = {}
+def compare_results(result, pattern):
+    equals = 0.
+    for p in pattern.keys():
+        if p in result:
+            equals += 1.
+    return equals / len(pattern)
 
-    # 4c (index, string) :
-    segment_substr_0_9 = {}
 
-    # 4d (index,  extension) :
-    segment_ext = {}
+INPUT_PATH = './data/'
+CHECK_PATH = './check/'
 
-    # 4e (index, string) :
-    segment_ext_substr_0_9 = {}
 
-    # 4d (index,  len) :
-    segment_len = {}
+if not os.path.exists(INPUT_PATH):
+    print >> sys.stderr, "Missing input path " + INPUT_PATH
+    sys.exit(1)
 
-    def _add_feature(dict_name, key):
-        dict_name.setdefault(key, 0)
-        dict_name[key] += 1
 
-    def get_link_features(link):
-        parse_list = urlparse(link)
-        path = parse_list.path
-        if path[0] == "/":
-            path = path[1:]
-        if len(path) > 0 and path[-1] == "/":
-            path = path[:-1]
-        segment_names_list = path.split("/")
+if not os.path.exists(CHECK_PATH):
+    print >> sys.stderr, "Missing check path " + CHECK_PATH
+    sys.exit(1)
 
-        segments_number = len(segment_names_list)
-        _add_feature(segments, segments_number)
+    
+files = os.listdir(INPUT_PATH)
+files = sorted(files)
+names = map(extract_names, files)
 
-        query_dict = parse_qs(parse_list.query)
-        # not empty
-        if query_dict:
-            _add_feature(param_name, str(query_dict.keys()))
+count = len(names) / 2
+boot_strap_cnt = 100
+test_result = {}
 
-            for query in query_dict.items():
-                _add_feature(param, "{}={}".format(query[0], query[1][0]))
+for bs in range(0, boot_strap_cnt):
+    print "Bootstrap step: " + str(bs)
+    for i in range(0, count):
+        f1 = INPUT_PATH + names[i*2][2]
+        f2 = INPUT_PATH + names[i*2+1][2]
+        name = names[i*2][0]
+        out = CHECK_PATH + name + '.fea.res'
+        check_f = CHECK_PATH + name + '.fea'
+        if name not in test_result:
+            test_result[name] = (0, 0, 0, False)
+        if not os.path.exists(check_f):
+            print >> sys.stderr, "Output test file was not created"
+            continue
+        if os.path.exists(out):
+            os.remove(out)
 
-        contain_digit = False
-        for number, segment in enumerate(segment_names_list):
-            contain_digit = False
-            _add_feature(segment_name, (number, segment))
-            _add_feature(segment_len, (number, len(segment)))
-            if segment.find("%") >= 0:
-                try:
-                    segment = unquote(segment).decode("cp1251")
-                except:
-                    pass
-            if segment.isdigit():
-                _add_feature(segment_0_9, number)
-            elif len(re.findall(r'\d+', segment)) == 1:
-                _add_feature(segment_substr_0_9, number)
-                contain_digit = True
+        t1 = time.time() 
+        extract_features.extract_features(f1, f2, out)
+        t2 = time.time()
+        if not os.path.exists(out):
+            print >> sys.stderr, "Output test file was not created"
+            continue
+        result = read_feas(out)
+        pattern = read_feas(check_f)
+        t = test_result[name][0] + (t2-t1)
+        w = test_result[name][1] + compare_results(result, pattern)
+        c = test_result[name][2] + float(len(result)) / float(len(pattern))
 
-        last_segment_split = segment_names_list[-1].split(".")
-        contain_ext = False
-        if len(last_segment_split) > 1:
-            _add_feature(segment_ext, (segments_number - 1, last_segment_split[-1].lower()))
-            contain_ext = True
+        test_result[name] = (t, w, c, True)
+        
+count_fails = 0
+for name in test_result.keys():
+    out = name + " test: ( err: " + str(1-(test_result[name][1] / boot_strap_cnt)) + ") "
+    if test_result[name][0] / boot_strap_cnt > 1:
+        out += " fail due execution time. Average execution time: " + str(test_result[name][0] / boot_strap_cnt)
+        count_fails += 1
+    else:
+        if test_result[name][1] / boot_strap_cnt < 0.95:
+            out += " fail due features names. Err is more then 0.05"
+            count_fails += 1
+        else:
+            if test_result[name][2] / boot_strap_cnt > 1.1:
+                out += " fail due count of features: " + str(test_result[name][2] / boot_strap_cnt)
+                count_fails += 1
+            else:
+                if not test_result[name][3]:
+                    out += " fail due system errors"
+                    count_fails += 1
+                else:
+                    out += 'passed'
+    print out
 
-        if contain_digit and contain_ext:
-            # del segment_substr_0_9[segments_number - 1]
-            # del segment_ext[(segments_number - 1, last_segment_split[-1].lower())]
-            _add_feature(segment_ext_substr_0_9, (segments_number - 1, last_segment_split[-1].lower()))
-
-    def print_to_file():
-        threshold = LINK_QUANTITY * ALPHA
-        # threshold = 0
-        sort_features_dict = {}
-        # 1
-        for feature, number in segments.items():
-            if number > threshold:
-                sort_features_dict["segments:{}\t{}".format(feature, number)] = number
-        # 2
-        for feature, number in param_name.items():
-            if number > threshold:
-                sort_features_dict["param_name:{}\t{}".format(feature, number)] = number
-        # 3
-        for feature, number in param.items():
-            if number > threshold:
-                sort_features_dict["param:{}\t{}".format(feature, number)] = number
-        # 4a
-        for feature, number in segment_name.items():
-            if number > threshold:
-                sort_features_dict["segment_name_{}:{}\t{}".format(feature[0], feature[1], number)] = number
-        # 4b
-        for feature, number in segment_0_9.items():
-            if number > threshold:
-                sort_features_dict["segment_[0-9]_{}:1\t{}".format(feature, number)] = number
-        # 4c
-        for feature, number in segment_substr_0_9.items():
-            if number > threshold:
-                sort_features_dict["segment_substr[0-9]_{}:1\t{}".format(feature, number)] = number
-        # 4d
-        for feature, number in segment_ext.items():
-            if number > threshold:
-                sort_features_dict["segment_ext_{}:{}\t{}".format(feature[0], feature[1], number)] = number
-        # 4e
-        # for feature, number in segment_ext_substr_0_9.items():
-        #     if number > threshold:
-        #         sort_features_dict["segment_ext_substr_[0-9]_{}:{}\t{}".format(feature[0], feature[1], number)] = number
-        # 4f
-        for feature, number in segment_len.items():
-            if number > threshold:
-                sort_features_dict["segment_len_{}:{}\t{}".format(feature[0], feature[1], number)] = number
-
-        sort_features_dict = sorted(sort_features_dict, key=sort_features_dict.get, reverse=True)
-        for feature in sort_features_dict:
-            out.write(feature + '\n')
-
-    # def load_links(_file, quantity):
-    #     line_number = 0
-    #     for _ in _file:
-    #         line_number += 1
-    #     loaded = 0
-    #     shift = 0
-    #     step = random.randint(2, 10)
-    #     while loaded < quantity:
-    #         for number, line in enumerate(_file):
-    #             if (number + shift) % step != 0:
-    #                 continue
-    #             get_link_features(line[:-1])
-    #             loaded += 1
-    #             if loaded >= quantity:
-    #                 break
-    #         _file.seek(0)
-    #         shift += 1
-
-    # def load_links(_file, quantity):
-    #     loaded = 0
-    #     while loaded < quantity:
-    #         step = random.randint(1, 30)
-    #         for number, line in enumerate(_file):
-    #             if number % step != 0:
-    #                 continue
-    #             get_link_features(line[:-1])
-    #             loaded += 1
-    #             if loaded >= quantity:
-    #                 break
-    #         _file.seek(0)
-
-    # # for any length file
-    # def load_links(_file, quantity):
-    #     loaded = 0
-    #     step = 1
-    #     for number, line in enumerate(_file):
-    #         if number % step != 0:
-    #             continue
-    #         get_link_features(line[:-1])
-    #         loaded += 1
-    #         if loaded >= quantity:
-    #             break
-    #     _file.seek(0)
-
-    def load_links(_file, quantity):
-        line_number = 0
-        for _ in _file:
-            line_number += 1
-        load_lines = np.sort(np.random.choice(line_number, quantity, replace=False))
-        index = 0
-        current_position = load_lines[index]
-        _file.seek(0)
-        for number, line in enumerate(_file):
-            if number != current_position:
-                continue
-            get_link_features(line[:-1])
-            index += 1
-            if index >= quantity:
-                break
-            current_position = load_lines[index]
-
-    q_link = open(INPUT_FILE_1, "r")
-    all_link = open(INPUT_FILE_2, "r")
-    out = open(OUTPUT_FILE, "w")
-
-    load_links(q_link, LINK_QUANTITY)
-    load_links(all_link, LINK_QUANTITY)
-
-    print_to_file()
-
-    q_link.close()
-    all_link.close()
-    out.close()
+if count_fails > 0: 
+    print 'Total fails: ' + str(count_fails) + " NOT PASSED"
+else:
+    print 'PASSED'
--- HA_1/extract_features.py
+++ HA_1/extract_features.py
@@ -1,8 +1,7 @@
 # coding: utf-8
 import random
 import re
-import sys
-from string import digits
+import numpy as np
 from urlparse import urlparse, parse_qs, unquote
 
 LINK_QUANTITY = 1000
@@ -32,7 +31,8 @@ def extract_features(INPUT_FILE_1, INPUT_FILE_2, OUTPUT_FILE):
     # 4d (index,  extension) :
     segment_ext = {}
 
-    # todo 4e
+    # 4e (index, string) :
+    segment_ext_substr_0_9 = {}
 
     # 4d (index,  len) :
     segment_len = {}
@@ -59,9 +59,11 @@ def extract_features(INPUT_FILE_1, INPUT_FILE_2, OUTPUT_FILE):
             _add_feature(param_name, str(query_dict.keys()))
 
             for query in query_dict.items():
-                _add_feature(param, "{}={}".format(query[0], query[1]))
+                _add_feature(param, "{}={}".format(query[0], query[1][0]))
 
+        contain_digit = False
         for number, segment in enumerate(segment_names_list):
+            contain_digit = False
             _add_feature(segment_name, (number, segment))
             _add_feature(segment_len, (number, len(segment)))
             if segment.find("%") >= 0:
@@ -73,63 +75,124 @@ def extract_features(INPUT_FILE_1, INPUT_FILE_2, OUTPUT_FILE):
                 _add_feature(segment_0_9, number)
             elif len(re.findall(r'\d+', segment)) == 1:
                 _add_feature(segment_substr_0_9, number)
+                contain_digit = True
 
         last_segment_split = segment_names_list[-1].split(".")
+        contain_ext = False
         if len(last_segment_split) > 1:
             _add_feature(segment_ext, (segments_number - 1, last_segment_split[-1].lower()))
+            contain_ext = True
+
+        if contain_digit and contain_ext:
+            # del segment_substr_0_9[segments_number - 1]
+            # del segment_ext[(segments_number - 1, last_segment_split[-1].lower())]
+            _add_feature(segment_ext_substr_0_9, (segments_number - 1, last_segment_split[-1].lower()))
 
     def print_to_file():
         threshold = LINK_QUANTITY * ALPHA
+        # threshold = 0
         sort_features_dict = {}
         # 1
         for feature, number in segments.items():
-            if number >= threshold:
+            if number > threshold:
                 sort_features_dict["segments:{}\t{}".format(feature, number)] = number
         # 2
         for feature, number in param_name.items():
-            if number >= threshold:
+            if number > threshold:
                 sort_features_dict["param_name:{}\t{}".format(feature, number)] = number
         # 3
         for feature, number in param.items():
-            if number >= threshold:
+            if number > threshold:
                 sort_features_dict["param:{}\t{}".format(feature, number)] = number
         # 4a
         for feature, number in segment_name.items():
-            if number >= threshold:
+            if number > threshold:
                 sort_features_dict["segment_name_{}:{}\t{}".format(feature[0], feature[1], number)] = number
         # 4b
         for feature, number in segment_0_9.items():
-            if number >= threshold:
+            if number > threshold:
                 sort_features_dict["segment_[0-9]_{}:1\t{}".format(feature, number)] = number
         # 4c
         for feature, number in segment_substr_0_9.items():
-            if number >= threshold:
+            if number > threshold:
                 sort_features_dict["segment_substr[0-9]_{}:1\t{}".format(feature, number)] = number
         # 4d
         for feature, number in segment_ext.items():
-            if number >= threshold:
+            if number > threshold:
                 sort_features_dict["segment_ext_{}:{}\t{}".format(feature[0], feature[1], number)] = number
+        # 4e
+        # for feature, number in segment_ext_substr_0_9.items():
+        #     if number > threshold:
+        #         sort_features_dict["segment_ext_substr_[0-9]_{}:{}\t{}".format(feature[0], feature[1], number)] = number
         # 4f
         for feature, number in segment_len.items():
-            if number >= threshold:
+            if number > threshold:
                 sort_features_dict["segment_len_{}:{}\t{}".format(feature[0], feature[1], number)] = number
 
         sort_features_dict = sorted(sort_features_dict, key=sort_features_dict.get, reverse=True)
         for feature in sort_features_dict:
             out.write(feature + '\n')
 
-    def load_links(file, quantity):
-        loaded = 0
-        while loaded < quantity:
-            step = random.randint(1, 5)
-            for number, line in enumerate(file):
-                if number % step != 0:
-                    continue
-                get_link_features(line[:-1])
-                loaded += 1
-                if loaded >= quantity:
-                    break
-            file.seek(0)
+    # def load_links(_file, quantity):
+    #     line_number = 0
+    #     for _ in _file:
+    #         line_number += 1
+    #     loaded = 0
+    #     shift = 0
+    #     step = random.randint(2, 10)
+    #     while loaded < quantity:
+    #         for number, line in enumerate(_file):
+    #             if (number + shift) % step != 0:
+    #                 continue
+    #             get_link_features(line[:-1])
+    #             loaded += 1
+    #             if loaded >= quantity:
+    #                 break
+    #         _file.seek(0)
+    #         shift += 1
+
+    # def load_links(_file, quantity):
+    #     loaded = 0
+    #     while loaded < quantity:
+    #         step = random.randint(1, 30)
+    #         for number, line in enumerate(_file):
+    #             if number % step != 0:
+    #                 continue
+    #             get_link_features(line[:-1])
+    #             loaded += 1
+    #             if loaded >= quantity:
+    #                 break
+    #         _file.seek(0)
+
+    # # for any length file
+    # def load_links(_file, quantity):
+    #     loaded = 0
+    #     step = 1
+    #     for number, line in enumerate(_file):
+    #         if number % step != 0:
+    #             continue
+    #         get_link_features(line[:-1])
+    #         loaded += 1
+    #         if loaded >= quantity:
+    #             break
+    #     _file.seek(0)
+
+    def load_links(_file, quantity):
+        line_number = 0
+        for _ in _file:
+            line_number += 1
+        load_lines = np.sort(np.random.choice(line_number, quantity, replace=False))
+        index = 0
+        current_position = load_lines[index]
+        _file.seek(0)
+        for number, line in enumerate(_file):
+            if number != current_position:
+                continue
+            get_link_features(line[:-1])
+            index += 1
+            if index >= quantity:
+                break
+            current_position = load_lines[index]
 
     q_link = open(INPUT_FILE_1, "r")
     all_link = open(INPUT_FILE_2, "r")
@@ -137,6 +200,7 @@ def extract_features(INPUT_FILE_1, INPUT_FILE_2, OUTPUT_FILE):
 
     load_links(q_link, LINK_QUANTITY)
     load_links(all_link, LINK_QUANTITY)
+
     print_to_file()
 
     q_link.close()
